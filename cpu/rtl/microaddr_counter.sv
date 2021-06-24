4742207d64ac0f29d742c9b69637c7df06ddcfbf
Daniel Cliche <dcliche@meldora.com>
2021-06-24 09:39:18 -0400

    Added micro opcodes

--- cpu/rtl/microaddr_counter.sv
+++ cpu/rtl/microaddr_counter.sv
@@ -1,32 +1,73 @@
+import microaddr_types::uaddr_t;
+
 module microaddr_counter(
-    input logic clk,
-    input logic reset,
-    input microaddr::cmd cmd,
-    input logic[10:0] load_addr,
-    output logic[10:0] addr
+    input bit clk,
+    input bit reset,
+    input bit zflag,
+    input byte unsigned M,
+    input microaddr_types::cmd_t cmd,
+    input uaddr_t load_addr,
+    output uaddr_t addr
 );
 
-logic[10:0] next_addr;
+parameter opjump_table_file="../data/opjump_table_test.data";
+
+uaddr_t next_addr;
+uaddr_t call_stack[4];
+uaddr_t next_call_stack[4];
+bit[1:0] call_stack_ptr;
+bit[1:0] next_call_stack_ptr;
+
+uaddr_t opjump_table[256];
+initial $readmemh(opjump_table_file, opjump_table);
 
 always_comb begin
+    next_call_stack_ptr = call_stack_ptr;
+    next_call_stack = call_stack;
+
     unique case(cmd)
-        microaddr::NONE:
+        microaddr_types::NONE:
             next_addr = addr;
-        microaddr::INC:
+
+        microaddr_types::INC:
             next_addr = addr + 1;
-        microaddr::LOAD:
+
+        microaddr_types::JMP:
             next_addr = load_addr;
+
+        microaddr_types::JNZ:
+            next_addr = zflag == 0 ? load_addr : addr + 1;
+
+        microaddr_types::CALL: begin
+            next_call_stack[call_stack_ptr] = addr + 1;
+            next_call_stack_ptr = call_stack_ptr + 1;
+            next_addr = load_addr;
+        end
+
+        microaddr_types::RET: begin
+            next_addr = call_stack[call_stack_ptr - 1];
+            next_call_stack_ptr = call_stack_ptr - 1;
+        end
+
+        microaddr_types::OPJMP:
+            next_addr = opjump_table[M];
+
         default:
             next_addr = 0;
+
     endcase // cmd
 
-    if (reset == 1)
+    if (reset == 1) begin
         next_addr = 0;
+        next_call_stack_ptr = 0;
+    end
 
 end // always_comb
 
 always_ff @(posedge clk) begin
     addr <= next_addr;
+    call_stack_ptr <= next_call_stack_ptr;
+    call_stack <= next_call_stack;
 end // always_ff
 
 endmodule // microaddr_counter
--- cpu/sim/sim_cpu.cpp
+++ cpu/sim/sim_cpu.cpp
@@ -6,47 +6,87 @@
 #include <verilated.h>
 #include <iostream>
 
-#include "Vmicroaddr_microaddr.h"
+#include "Vmicroaddr_microaddr_types.h"
 #include "Vmicroaddr.h"
 
 struct TestCase
 {
     const char *name;
     uint8_t reset;
+    uint8_t zflag;
+    uint8_t M;
     uint8_t cmd;
     uint16_t load_addr;
     uint16_t expected_addr;
 };
 
+using cmd_t = Vmicroaddr_microaddr_types::cmd_t;
+
 TestCase test_cases[]{
-    {"reset", 1, Vmicroaddr_microaddr::cmd::NONE, 0, 0},
-    {"inc", 0, Vmicroaddr_microaddr::cmd::INC, 0, 1},
-    {"none", 0, Vmicroaddr_microaddr::cmd::NONE, 0, 1},
-    {"reset2", 1, Vmicroaddr_microaddr::cmd::NONE, 0, 0},
-    {"load", 0, Vmicroaddr_microaddr::cmd::LOAD, 0xFA, 0xFA},
-    {"inc", 0, Vmicroaddr_microaddr::cmd::INC, 0, 0xFB},
-    {"reset3", 1, Vmicroaddr_microaddr::cmd::INC, 0, 0},
-};
+    {"reset", 1, 0, 0, cmd_t::NONE, 0, 0},
+    {"inc", 0, 0, 0, cmd_t::INC, 0, 1},
+    {"none", 0, 0, 0, cmd_t::NONE, 0, 1},
+    {"reset2", 1, 0, 0, cmd_t::NONE, 0, 0},
+    {"jmp", 0, 0, 0, cmd_t::JMP, 0xFA, 0xFA},
+    {"inc", 0, 0, 0, cmd_t::INC, 0, 0xFB},
+    {"reset3", 1, 0, 0, cmd_t::INC, 0, 0},
+
+    {"jmpF", 0, 0, 0, cmd_t::JMP, 0xFFFF, 0x7FF},
+
+    {"reset_call", 1, 0, 0, cmd_t::NONE, 0, 0},
+    {"call1", 0, 0, 0, cmd_t::CALL, 0xA0, 0xA0},
+    {"ret1", 0, 0, 0, cmd_t::RET, 0, 0x01},
+
+    {"reset4", 1, 0, 0, cmd_t::NONE, 0, 0},
+    {"call2.1", 0, 0, 0, cmd_t::CALL, 0xA0, 0xA0},
+    {"call2.2", 0, 0, 0, cmd_t::CALL, 0xB0, 0xB0},
+    {"ret2.2", 0, 0, 0, cmd_t::RET, 0, 0xA1},
+    {"ret2.1", 0, 0, 0, cmd_t::RET, 0, 0x01},
+
+    {"reset5", 1, 0, 0, cmd_t::NONE, 0, 0},
+    {"jnz", 0, 1, 0, cmd_t::JNZ, 0xA0, 0x01},
+    {"jnz2", 0, 0, 0, cmd_t::JNZ, 0xA0, 0xA0},
+
+    {"opjmp", 0, 0, 1, cmd_t::OPJMP, 0, 0x1AB},
+    {"opjmp2", 0, 0, 2, cmd_t::OPJMP, 0, 0x012},
+    {"opjmp3", 0, 0, 3, cmd_t::OPJMP, 0, 0}};
+
+bool test_expected_addr(Vmicroaddr *counter, const TestCase &test_case)
+{
+    if (counter->addr == test_case.expected_addr)
+    {
+        return true;
+    }
+    printf("%s: fail: expected addr %08X but was %08X\n",
+           test_case.name, test_case.expected_addr,
+           counter->addr);
+    return false;
+}
 
 int main(int argc, char **argv, char **env)
 {
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
     contextp->commandArgs(argc, argv);
     contextp->randReset(2);
-    const std::unique_ptr<Vmicroaddr> top{new Vmicroaddr{contextp.get(), "TOP"}};
+    Vmicroaddr *top = new Vmicroaddr{contextp.get(), "TOP"};
 
     top->clk = 0;
     top->reset = 0;
-    top->cmd = Vmicroaddr_microaddr::cmd::NONE;
+    top->zflag = 0;
+    top->M = 0;
+    top->cmd = cmd_t::NONE;
     top->load_addr = 0;
     top->eval();
 
     int num_test_cases = sizeof(test_cases) / sizeof(TestCase);
+    int num_passes = 0;
 
     for (const auto &test_case : test_cases)
     {
         top->cmd = test_case.cmd;
         top->reset = test_case.reset;
+        top->zflag = test_case.zflag;
+        top->M = test_case.M;
         top->load_addr = test_case.load_addr;
         top->eval();
 
@@ -55,18 +95,20 @@ int main(int argc, char **argv, char **env)
         top->clk = 0;
         top->eval();
 
-        if (top->addr == test_case.expected_addr)
+        bool passed = true;
+        passed &= test_expected_addr(top, test_case);
+
+        if (passed)
         {
-            printf("%s passed\n", test_case.name);
-        }
-        else
-        {
-            printf("%s fail (expected %04X but was %04X)\n", test_case.name, test_case.expected_addr, top->addr);
+            printf("%s: passed\n", test_case.name);
+            num_passes++;
         }
     }
 
     // Final model cleanup
     top->final();
-
+    delete top;
+    printf("\n");
+    printf("Tests passed: %d/%d\n", num_passes, num_test_cases);
     return 0;
 }
