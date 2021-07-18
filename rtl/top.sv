abcdef24ecddf44df24c1e98c08cac1aecd49a31
Daniel Cliche <dcliche@meldora.com>
2021-07-18 10:55:56 -0400

    Added bouncing ball and paddles

--- rtl/top.sv
+++ rtl/top.sv
@@ -34,45 +34,102 @@ module top(
    );
 
    //
-   // Bouncing square
+   // Pong
+   // Ref.: https://projectf.io/posts/fpga-pong/
    //
 
+   // size of the screen with and without blanking
    localparam H_RES_FULL = 800;
    localparam V_RES_FULL = 525;
    localparam H_RES = 640;
    localparam V_RES = 480;
 
-   logic animate;
+   logic animate; // high for one clock ticj at the start of vertical blanking
    always_comb animate = (sy == V_RES && sx == 0);
 
-   localparam Q_SIZE = 32;
-   localparam Q_SPEED = 4;
-   logic[CORDW-1:0] qx, qy;
+   // ball
+   localparam B_SIZE = 8;     // size in pixels
+   logic [CORDW-1:0] bx, by;  // position
+   logic dx, dy;              // direction: 0 is right/down
+   logic [CORDW-1:0] spx = 1; // horizontal speed
+   logic [CORDW-1:0] spy = 1; // vertical speed
+   logic b_draw;              // draw ball?
 
+   // ball animation
    always_ff @(posedge clk_pix) begin
       if (animate) begin
-         if (qx >= H_RES_FULL - Q_SIZE) begin
-            qx <= 0;
-            qy <= (qy >= V_RES_FULL - Q_SIZE) ? 0 : qy + Q_SIZE;         
-         end else begin
-            qx <= qx + Q_SPEED;
+         // Horizontal
+         if (bx >= H_RES - (spx + B_SIZE)) begin   // right edge
+            dx <= 1;
+            bx <= bx - spx;
+         end else if (bx < spx) begin  // left edge
+            dx <= 0;
+            bx <= bx + spx;
+         end else bx <= (dx) ? bx - spx : bx + spx;
+
+         // Vertical
+         if (by >= V_RES - (spy + B_SIZE)) begin   // bottom edge
+            dy <= 1;
+            by <= by - spy;
+         end else if (by < spy) begin  // top edge
+            dy <= 0;
+            by <= by + spy;
+         end else by <= (dy) ? by - spy : by + spy;
+      end
+   end
+
+   // draw ball - is ball at current screen position?
+   always_comb begin
+      b_draw = (sx >= bx) && (sx < bx + B_SIZE) &&
+               (sy >= by) && (sy < by + B_SIZE);
+   end
+
+   // paddles
+   localparam P_H = 40;          // height in pixels
+   localparam P_W = 10;          // width in pixels
+   localparam P_SP = 1;          // speed
+   localparam P_OFFS = 32;       // offset from screen edge
+   logic [CORDW-1:0] p1y, p2y;   // vertical position of paddles 1 and 2
+   logic p1_draw, p2_draw;       // draw paddles?
+
+   // paddle animation
+   always_ff @(posedge clk_pix) begin
+      if (animate) begin
+         // "AI" paddle 1
+         if ((p1y + P_H/2) + P_SP/2 < (by + B_SIZE/2)) begin
+            if (p1y < V_RES - (P_H + P_SP/2))   // screen bottom?
+               p1y <= p1y + P_SP;               // move down
+         end else if ((p1y + P_H/2) > (by + B_SIZE/2) + P_SP/2) begin
+            if (p1y > P_SP)                     // screen top?
+               p1y <= p1y - P_SP;               // move up
+         end
+
+         // "AI" paddle 2
+         if ((p2y + P_H/2) + P_SP/2 < (by + B_SIZE/2)) begin
+            if (p2y < V_RES - (P_H + P_SP/2))   // screen bottom?
+               p2y <= p2y + P_SP;               // move down
+         end else if ((p2y + P_H/2) > (by + B_SIZE/2) + P_SP/2) begin
+            if (p2y > P_SP)                     // screen top?
+               p2y <= p2y - P_SP;               // move up
          end
       end
    end
 
-   logic q_draw;
+   // draw paddles - are paddles at current screen position?
    always_comb begin
-      q_draw = (sx >= qx) && (sx < qx + Q_SIZE) &&
-               (sy >= qy) && (sy < qy + Q_SIZE);
+      p1_draw = (sx >= P_OFFS) && (sx < P_OFFS + P_W) &&
+                (sy >= p1y) && (sy < p1y + P_H);
+      p2_draw = (sx >= H_RES - P_OFFS - P_W) && (sx < H_RES - P_OFFS) &&
+                (sy >= p2y) && (sy < p2y + P_H);
    end
 
    // VGA output
    always_ff @(posedge clk_pix) begin
       vga_hsync <= hsync;
       vga_vsync <= vsync;
-      vga_r <= !de ? 4'h0 : (q_draw ? 4'hF : 4'h0);
-      vga_g <= !de ? 4'h0 : (q_draw ? 4'h8 : 4'h8);
-      vga_b <= !de ? 4'h0 : (q_draw ? 4'h0 : 4'hF);
+      vga_r <= (de && (b_draw || p1_draw || p2_draw)) ? 4'hF : 4'h0;
+      vga_g <= (de && (b_draw || p1_draw || p2_draw)) ? 4'hF : 4'h0;
+      vga_b <= (de && (b_draw || p1_draw || p2_draw)) ? 4'hF : 4'h0;
    end
 
    //
@@ -80,10 +137,10 @@ module top(
    //
 
    logic [7:0] cpu_data_in, cpu_data_out;
-   logic [5:0] addr = 0;
+   logic [5:0] addr;
    logic [11:0] mem_addr;
    logic rw = 1;
-   logic [7:0] display = 0;
+   logic [7:0] display;
    logic ram_cs;
 
    ram #(.A(12), .D(8)) ram0(
